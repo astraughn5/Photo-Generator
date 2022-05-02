721d8b281de50482147d88624b3afbfe10d73e0f
astraughn5 <astraughn5@gmail.com>
2022-05-02 12:21:56 -0500

    updated to count the # of likes/dislikes w/ API key

--- main_sample.js
+++ main_sample.js
@@ -1,14 +1,65 @@
 var token = configKey.MY_API_TOKEN;
 
 let photoGenerator = `https://api.unsplash.com/photos/random/?client_id=${token}`;
+let image = document.querySelector('#unsplashImage');
+let photographer = document.querySelector('#photographer');
+let dislikeCounter = document.querySelector('#dislikeCounter');
+let likeCounter = document.querySelector("#likeCounter");
+let dislikeCount = 0;
+let likeCount = 0;
+
+const getPhotoForDislike = function(){
+	dislikeCount += 1
+	dislikeCounter.innerText = dislikeCount
+	console.log(dislikeCount)
 
 	fetch(photoGenerator)
 		.then(res => res.json()) // parse response as JSON
 		.then(data => {
-		console.log(data)	
-		document.body.style.backgroundImage = data.urls.regular
-		document.querySelector('img').src = data.urls.regular
+		// console.log(data)	
+		image.src = data.urls.regular
+		photographer.innerText = data.user.name
+		photographer.setAttribute("href", data.user.portfolio)
 		})
 		.catch(err => {
 			console.log(`error ${err}`)
 		});
+
+}
+const getPhotoForLike = function(){
+	likeCount += 1
+	likeCounter.innerText = likeCount
+	console.log(likeCount)
+
+	fetch(photoGenerator)
+		.then(res => res.json()) // parse response as JSON
+		.then(data => {
+		// console.log(data)	
+		image.src = data.urls.regular
+		photographer.innerText = data.user.name
+		photographer.setAttribute("href", data.user.portfolio)
+		})
+		.catch(err => {
+			console.log(`error ${err}`)
+		});
+
+}
+
+const getPhoto = function(){
+	fetch(photoGenerator)
+		.then(res => res.json()) // parse response as JSON
+		.then(data => {
+		// console.log(data)	
+		image.src = data.urls.regular
+		photographer.innerText = data.user.name
+		photographer.setAttribute("href", data.user.portfolio)
+		})
+		.catch(err => {
+			console.log(`error ${err}`)
+		});
+
+}
+
+document.getElementById('dislikeTrigger').addEventListener('click',getPhotoForDislike);
+document.getElementById('likeTrigger').addEventListener('click',getPhotoForLike);
+window.addEventListener('load',getPhoto);
